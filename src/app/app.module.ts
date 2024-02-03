43ca47a7010743fbbf360a883bf48ab223bea0f6
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-02-03 15:54:34 +0100

    add user button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -23,7 +27,9 @@ import { UserComponent } from './user/user.component';
     MatToolbarModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
