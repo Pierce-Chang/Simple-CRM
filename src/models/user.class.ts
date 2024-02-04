e2ca03a643e7d9695b9ec18bb5b5a82556427154
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-02-04 21:59:58 +0100

    added email element and logic

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -6,6 +6,7 @@ export class User {
     }
     firstName: string;
     lastName: string;
+    email: string;
     birthDate: number;
     street: string;
     zipCode: number;
@@ -14,6 +15,7 @@ export class User {
     constructor(obj?: any) {
         this.firstName = obj ? obj.firstName : '';
         this.lastName = obj ? obj.lastName : '';
+        this.email = obj ? obj.email : '';
         this.birthDate = obj ? obj.birthDate : '';
         this.street = obj ? obj.street : '';
         this.zipCode = obj ? obj.zipCode : '';
@@ -24,6 +26,7 @@ export class User {
         return {
         firstName: this.firstName,
         lastName: this.lastName,
+        email: this.email,
         birthDate: this.birthDate,
         street: this.street,
         zipCode: this.zipCode,
